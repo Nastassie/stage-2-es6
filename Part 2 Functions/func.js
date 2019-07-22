52e56596911ffa12cca1e23f227f07488167bbdc
Nastassie <50500307+Nastassie@users.noreply.github.com>
2019-07-22 10:55:20 +0300

    Update func.js

--- Part 2 Functions/func.js
+++ Part 2 Functions/func.js
@@ -1,19 +1,11 @@
-var arr = [1, 1, 1, 1, 1, -7, 1, 1, 1, 1];
-
 function process(arr)
 {
     function sortnumbers(x, y) { return x-y; }
     const sortarr = arr.sort(sortnumbers)
 
-    //console.log(sortarr);
     var a = sortarr[0];
     var b = sortarr[1];
     var c = sortarr[sortarr.length-1];
-
-    //console.log(a);
-    //console.log(b);
-    //console.log(c);
-
 
     if (a == b) {
         return c;   
@@ -21,24 +13,25 @@ function process(arr)
 
     return a;
 }
-
+var arr = [1, 1, 1, 1, 1, -7, 1, 1, 1, 1];
 console.log(process(arr));
 
 
 function superSort(arrwords) { arrwords.sort((a, b) => a.replace(/[0-9]/g, '').localeCompare(b.replace(/[0-9]/g, ''))) }
  
 arrword = ['mic09ha1el', '4b5en6', 'michelle', 'be4atr3ice'];
-superSort(arrword)
-console.log(arrword)
+//console.log(arrword);
+superSort(arrword);
+console.log(arrword);
 
 function dateComparison(date1, format1, date2, format2) {
     
 
     function parseDate(input, format) {
-         format = format || 'dd/mm/yyyy'; // somedefault format
+         format = format || 'dd/mm/yyyy'; 
          var parts = input.match(/(\d+)/g),
              i = 0, fmt = {};
-  // extract date-part indexes from the format
+
          format.replace(/(yyyy|dd|mm)/g, function(part) { fmt[part] = i++; });
          return new Date(parts[fmt['yyyy']], parts[fmt['mm']]-1,   parts[fmt['dd']]);
     }
@@ -57,6 +50,6 @@ d5 = "07/05/1960";
 d6 = "08,05,2011";
 d7 = "2017.10.20";
 
-console.log(dateComparison(d1, "dd-mm-yyyy", d4, "dd-mm-yyyy"));
+console.log(dateComparison(d3, "dd-mm-yyyy", d4, "dd-mm-yyyy"));
 
 
